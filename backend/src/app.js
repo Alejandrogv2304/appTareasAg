ecde501b80fb2db284f09dccb377de283d1e989c
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2024-12-30 21:34:44 -0500

    Agregando funciones a los controladores
    Le agregué funciones a los controladores y probé las rutas

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import tasksRoutes from './routes/tasks';
 
 const app = express();
 
-
+app.use(express.json());//Para que express pueda leer el Json
 app.use(tasksRoutes);
 
 export default app;
--- backend/src/controllers/tasks.js
+++ backend/src/controllers/tasks.js
@@ -1,30 +1,43 @@
-import {connect} from '../databse';
+import {connect} from '../database';
 
 //Aquí definimos los métodos que se usan en el Routes
 export const getTasks = async (req,res) => {
-const db = await connect()
-const [rows]= await db.query('SELECT * FROM tasks')
+const connection = await connect()
+const [rows]= await connection.query('SELECT * FROM tasks')
 console.log(rows)
+res.json(rows)
+}
+
+export const getTask = async (req,res) => {
+const connection = await connect()
+const [rows] = await connection.query('SELECT * FROM tasks WHERE id = ?',
+    [
+    req.params.id
+    ]);
+res.json(rows[0])
+}
+
+ export const getTaskCount = async (req,res) => {
+ const connection = await connect()
+ const [rows] = await connection.query('SELECT count(*) FROM tasks');
+
+ res.json(rows[0]["count(*)"])
+}
+
+export const saveTask = async (req,res) => {
+const connection = await connect()
+const result = await connection.query('INSERT INTO tasks (title, description) VALUES (?, ?)', [
+    req.body.title,
+    req.body.description
+])
+console.log(result)
+}
+
+export const updateTask = async (req,res) => {
 res.send('Respuesta de Tasks de Alejandro');
 }
 
-export const getTask = (req,res) => {
-res.send('Respuesta de Tasks de Alejandro');
-}
-
- export const getTaskCount = (req,res) => {
- res.send('Respuesta de Tasks de Alejandro');
-}
-
-export const saveTask = (req,res) => {
-res.send('Respuesta de Tasks de Alejandro');
-}
-
-export const updateTask = (req,res) => {
-res.send('Respuesta de Tasks de Alejandro');
-}
-
-export const deleteTask = (req,res) => {
+export const deleteTask = async (req,res) => {
     res.send('Respuesta de Tasks de Alejandro');
     }
     
